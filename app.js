a8b9fb2d44d64b00ef9851e109739d0a6125b4ab
akanimoh <oakanimoh@gmail.com>
2023-02-07 18:34:43 +0100

    More examples on Node-js

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * let time = 0;
+
+var timer = setInterval(function(){
+    time+=2;
+    console.log(time + ' Seconds have passed');
+    if (time>5) {
+        clearInterval(timer);
+    }
+},2000);
+ */     
+console.log(__dirname);
